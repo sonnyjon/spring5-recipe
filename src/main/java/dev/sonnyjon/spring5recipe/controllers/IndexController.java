652cd57a616c6ce015fc08effe8805133df7f3b4
sonnyjon <sonny.s.jon@gmail.com>
2022-05-31 16:24:37 -0700

    JPA Query Methods for Category and Unit of Measure

--- src/main/java/dev/sonnyjon/spring5recipe/controllers/IndexController.java
+++ src/main/java/dev/sonnyjon/spring5recipe/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package dev.sonnyjon.spring5recipe.controllers;
 
+import dev.sonnyjon.spring5recipe.model.Category;
+import dev.sonnyjon.spring5recipe.model.UnitOfMeasure;
+import dev.sonnyjon.spring5recipe.repos.CategoryRepository;
+import dev.sonnyjon.spring5recipe.repos.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by Sonny on 5/25/2022.
@@ -9,9 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController
 {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository)
+    {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage()
     {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/dev/sonnyjon/spring5recipe/repos/UnitOfMeasureRepository.java
+++ src/main/java/dev/sonnyjon/spring5recipe/repos/UnitOfMeasureRepository.java
@@ -3,9 +3,12 @@ package dev.sonnyjon.spring5recipe.repos;
 import dev.sonnyjon.spring5recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Sonny on 5/31/2022.
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>
 {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }

3907b2c15de54706f90b7397505c09339ee8ff5e
sonnyjon <sonny.s.jon@gmail.com>
2022-05-31 15:23:33 -0700

    Many to many JPA relationships

--- src/main/java/dev/sonnyjon/spring5recipe/model/Recipe.java
+++ src/main/java/dev/sonnyjon/spring5recipe/model/Recipe.java
@@ -33,6 +33,14 @@ public class Recipe
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(
+            name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
+
     public Long getId()
     {
         return id;
@@ -151,5 +159,15 @@ public class Recipe
     public void setNotes(Notes notes)
     {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories()
+    {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories)
+    {
+        this.categories = categories;
     }
 }

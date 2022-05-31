eb5b2bfb1ca972073e6598d18d4b6ff8ebfa6c24
sonnyjon <sonny.s.jon@gmail.com>
2022-05-31 15:03:46 -0700

    Created enumerated type for Difficulty

--- src/main/java/dev/sonnyjon/spring5recipe/model/Recipe.java
+++ src/main/java/dev/sonnyjon/spring5recipe/model/Recipe.java
@@ -21,8 +21,8 @@ public class Recipe
     private String url;
     private String directions;
 
-    // TODO Difficulty
-    // private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -113,6 +113,26 @@ public class Recipe
         this.directions = directions;
     }
 
+    public Difficulty getDifficulty()
+    {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty)
+    {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients()
+    {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients)
+    {
+        this.ingredients = ingredients;
+    }
+
     public Byte[] getImage()
     {
         return image;
@@ -131,15 +151,5 @@ public class Recipe
     public void setNotes(Notes notes)
     {
         this.notes = notes;
-    }
-
-    public Set<Ingredient> getIngredients()
-    {
-        return ingredients;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients)
-    {
-        this.ingredients = ingredients;
     }
 }
